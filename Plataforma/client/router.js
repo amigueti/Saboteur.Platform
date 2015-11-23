31ef42acb9d4f95f26f33e8d7ae32dfae7f6d5a4
Rodrigo <r.arizadiaz@gmail.com>
2015-11-23 18:21:05 +0100

    añado sistema usuarios basico

--- Plataforma/client/router.js
+++ Plataforma/client/router.js
@@ -9,3 +9,5 @@ Router.route('/juegos', {name: 'juegos'});
 Router.route('/about',{name: 'about'});
 Router.route('/ranking',{name: 'ranking'});
 Router.route('/perfil',{name: 'perfil'});
+Router.route('/register',{name: 'register'});
+Router.route('/login',{name: 'login'});
