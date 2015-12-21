df6704eea3579520b13cca10cf528c0dc0904697
Rodrigo <r.arizadiaz@gmail.com>
2015-12-21 18:29:13 +0100

    añadidos juegos a BD

--- Plataforma/lib/router.js
+++ Plataforma/lib/router.js
@@ -1,15 +1,13 @@
-Router.configure({
+/*Router.configure({
   layoutTemplate: 'layout',
   loadingTemplate: 'loading',
   notFoundTemplate: 'notFound'
-});
+});*/
 
 Router.route('/', {name: 'home'});
 Router.route('/juegos', {name: 'juegos'});
 Router.route('/about',{name: 'about'});
 Router.route('/ranking',{name: 'ranking'});
-Router.route('/register',{name: 'register'});
-Router.route('/login',{name: 'login'});
 
 Router.route('/perfil',{
 	path: '/perfil/:id',
