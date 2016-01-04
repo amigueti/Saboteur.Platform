16d6933f81a20e287c9a0f3312f91886501847f7
allorentesa <alballorentesanchez@gmail.com>
2016-01-04 14:03:09 +0100

    ranking saboteur prueba

--- plt-saboteur/lib/models.js
+++ plt-saboteur/lib/models.js
@@ -4,6 +4,8 @@ Perfiles = new Meteor.Collection('perfiles');
 
 Amigos = new Meteor.Collection('amigos');
 
+Ranking = new Mongo.Collection("ranking");
+
 /* Modelos para el saboteur */
 Partidas = new Meteor.Collection('partidas');
 Acciones = new Meteor.Collection('acciones');
