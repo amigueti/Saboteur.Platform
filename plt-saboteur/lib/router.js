1fde28d457ca6f38722fb368eac4fc948f0fd8e1
Rodrigo <r.arizadiaz@gmail.com>
2016-01-07 23:51:30 +0100

    func sab en index

--- plt-saboteur/lib/router.js
+++ plt-saboteur/lib/router.js
@@ -1,17 +1,12 @@
-Router.configure({
+/*Router.configure({
   layoutTemplate: 'layout'
-});
+});*/
 
 Router.route('/perfil',{name: 'perfil'});
-Router.route('/',{name: 'chat'});
+Router.route('/');
 Router.route('/ranking',{name: 'ranking'});
 Router.route('/juego',{
 	template: 'saboteur'
 });
-<<<<<<< HEAD
 
 Router.route('/chatperfil',{name: 'chatperfil'});
-
-=======
-Router.route('/chatperfil',{name: 'chatperfil'});
->>>>>>> 7f551911c9bd0e4b8a72c5edd014f4250b53e86c
