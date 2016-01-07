7f551911c9bd0e4b8a72c5edd014f4250b53e86c
amigueti <jaimeamigueti@outlook.es>
2016-01-07 21:01:02 +0100

    chat privado casi terminado. template saboteur modificada, falta css

--- plt-saboteur/lib/router.js
+++ plt-saboteur/lib/router.js
@@ -1,17 +1,11 @@
-/*Router.configure({
+Router.configure({
   layoutTemplate: 'layout'
-});*/
+});
 
 Router.route('/perfil',{name: 'perfil'});
-
-
-Router.route('/');
+Router.route('/',{name: 'chat'});
 Router.route('/ranking',{name: 'ranking'});
 Router.route('/juego',{
 	template: 'saboteur'
 });
-<<<<<<< HEAD
 Router.route('/chatperfil',{name: 'chatperfil'});
-=======
-
->>>>>>> 31b94e1f62494a5460245ec00f2c1d9d6868ed3f
