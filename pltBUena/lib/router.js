2c26a497e3c2d9641212e2be117063f8a8f157ec
cherrerog <c.herrerog@alumnos.urjc.es>
2016-01-15 14:47:03 +0100

    Ranking subido + ccs mejorable + scroll amigos

--- pltBUena/lib/router.js
+++ pltBUena/lib/router.js
@@ -1,7 +1,7 @@
 Router.configure({
   layoutTemplate: 'layout',
-  notFoundTemplate: 'notFound',
-  loadingTemplate: 'loading'
+  notFoundTemplate: 'notFound'
+  
 });
 
 Router.route('/perfil',{name: 'perfil'});
